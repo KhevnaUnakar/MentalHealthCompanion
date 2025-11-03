3d0c22a9a7cde39683fd98c6b607f043f9dc4990
KhevnaUnakar <unakarkhevna@gmail.com>
2025-11-04 00:22:50 +0530

    setup env variables for secret key and modified html code. Open homepage on context path by setting up urls file.

--- mental_health_companion/mental_health_companion/settings.py
+++ mental_health_companion/mental_health_companion/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-os.getenv("OPENAI_API_KEY")
+#ENTER SECRET KEY HERE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
@@ -133,4 +133,4 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#ENTER API KEY HERE
